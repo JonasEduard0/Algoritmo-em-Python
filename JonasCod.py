1d3bf7ef3f3d30ca1e9d7b868aee62d17db4d622
Jonas E. C. Martins <106180613+JonasEduard0@users.noreply.github.com>
2022-10-02 13:44:22 -0300

    Add files via upload

--- JonasCod.py
+++ JonasCod.py
@@ -1,4 +1,4 @@
-'''     #Bibiotecas
+     #Bibiotecas
 from math import sqrt
 from time import sleep
 import random
@@ -12,7 +12,9 @@ raiz = sqrt(salario)
 print('Raiz de salario é', raiz)
 print("\n")
 
+
         #IF,else,elif
+
 vc = input("Digite a letra:")
 if vc == "a" or vc == "e" or vc == "i" or vc == "o" or vc == "u":
     print("Vogal")
@@ -20,7 +22,9 @@ if vc != "a" and vc != "e" and vc != "i" and vc != "o" and vc != "u":
     print("Consoante")
 print("\n")
 
+
         #For, laço de repetição
+
 i = int(input('I:'))
 f = int(input('F:'))
 p = int(input('P:'))
@@ -54,7 +58,9 @@ print('O homem mais velho tem {} anos e se chama {}'.format(idade, nomevelho))
 print('Ao todo sao {} mulheres com menos de 20 anos'.format(totmulher))
 print('\n')
 
+
         #While, laço de repetição
+
 co = 0
 while co < 10:
     co += 1
@@ -100,7 +106,9 @@ print(len(lanche))
 del(comida)
 print('\n')
 
+
         #Listas
+
 cont = 'zero','um','dois','tres','quatro','cinco'
 while True:
     nume = int(input('Numero entre 0 e 5: '))
@@ -123,7 +131,9 @@ for k, v in anime.items():  #items,values,keys
     print(f'O {k} é {v}')#k é keys e v é values
 print('\n')
 
+
         #Função
+
 def lin():
     print('-'*30)
 lin()
@@ -167,20 +177,22 @@ r2 = soma(1,7)
 r3 = soma(4)
 print(f'Os calculos deram {r},{r2} e {r3}')
 
+
         #Interactive help
 help()
 help(print)
+
 
         #Exceção/Erro
 try: 
     a = int(input('Numerador : '))
     b = int(input('Denominador : '))
-    r = a / b #Se b receber 0, dará um erro,com esse comando você pode exibir uma mensagem caso dê erro
-except Exception as erro: #Exception as variavel para mostrar o erro 
+    r = a / b               #Se b receber 0, dará um erro,com esse comando você pode exibir uma mensagem caso dê erro
+except Exception as erro:   #Exception as variavel para mostrar o erro 
     print(f'Houve um problema!Ele é {erro.__class__}') #Há erro.class, cause, context, etc
-except ValueError: #Pode ter uma mensagem expecífica para os erros
+except ValueError:          #Pode ter uma mensagem expecífica para os erros
     print('O erro é de valor')
 else:
     print(f'O resultado é {r}')
 finally:
-    print('Volte sempre!')'''
+    print('Volte sempre!')
